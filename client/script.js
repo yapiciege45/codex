3b4c873fea9cdda630fef8610524eebf83410f2c
Ege Yapıcı <65947689+yapiciege45@users.noreply.github.com>
2023-02-15 16:47:09 +0300

    Bugfix

--- client/script.js
+++ client/script.js
@@ -117,7 +117,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch(`https://codex-vinn.onrender.com/`, {
+    const response = await fetch(`https://codex-vinn.onrender.com`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
