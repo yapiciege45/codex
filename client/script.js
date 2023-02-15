d5502cd26422dbf6f12193be09217bf723ea5923
Ege Yapıcı <65947689+yapiciege45@users.noreply.github.com>
2023-02-15 15:46:54 +0300

    database change

--- client/script.js
+++ client/script.js
@@ -116,7 +116,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch(`https://codex-vinn.onrender.com`, {
+    const response = await fetch(`http://localhost:5000`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
