af4bde25a90caaf4d04fc88c205e9c6861ef8cdb
Ege Yapıcı <65947689+yapiciege45@users.noreply.github.com>
2023-02-15 16:30:30 +0300

    Sidebar changed

--- server/server.js
+++ server/server.js
@@ -20,6 +20,7 @@ const questionSchema = new Schema({
 const Question = mongoose.model('Question', questionSchema);
 
 const configuration = new Configuration({
+  organization: "org-nPBBuxo75dlipaYrGVGVyoiS",
   apiKey: "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",
 });
 
@@ -82,10 +83,7 @@ app.post('/', async (req, res) => {
       model: "text-davinci-003",
       prompt: `${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      max_tokens: 7, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
     });
 
     console.log({
